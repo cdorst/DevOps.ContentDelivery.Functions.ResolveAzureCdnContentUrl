de70b969ee0b3995bd82257d2d41c7077f48ffea
Christopher Dorst <chris@dorst.email>
2017-12-30 22:21:21 -0600

    Refactor blob URI-component responsibility into downstream package

--- DevOps.ContentDelivery.Functions.ResolveAzureCdnContentUrl/UrlResolver.cs
+++ DevOps.ContentDelivery.Functions.ResolveAzureCdnContentUrl/UrlResolver.cs
@@ -1,11 +1,10 @@
-﻿using DevOps.ContentDelivery.Structures.AzureBlob;
-using System;
+﻿using System;
 
 namespace DevOps.ContentDelivery.Functions.ResolveAzureCdnContentUrl
 {
     public static class UrlResolver
     {
-        public static Uri Url(string endpointName, Blob blob)
-            => new Uri($"https://{endpointName}.azureedge.net/{blob.Container}/{blob.Name}");
+        public static Uri Url(string endpointName, string path)
+            => new Uri($"https://{endpointName}.azureedge.net/{path}");
     }
 }
